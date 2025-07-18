f431ae267a00df3157f24cfbce2e8611613550fc
AgheraDhruv <dhruv.aghera.ce@gmail.com>
2025-07-18 16:11:05 +0530

    Add files via upload

---
+++ TURCOC3/INTRO.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("hii my name is aghera dhruv\n");
+printf("cspit\n");
+printf("ce\n");
+printf("rajkot\n");
+printf("utkarsh\n");
+getch();
+}
